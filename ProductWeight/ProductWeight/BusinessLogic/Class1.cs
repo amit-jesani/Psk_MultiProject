6e52e839a421deb2dece41635434273c34bb9c61
Pooja Kachhiya <pooja.kachhiya@dynatechconsultancy.com>
2024-12-09 11:48:36 +0530

    add diff method

--- ProductWeight/ProductWeight/BusinessLogic/Class1.cs
+++ ProductWeight/ProductWeight/BusinessLogic/Class1.cs
@@ -2,14 +2,18 @@
 
 public class Class1
 {
-	public Class1()
-	{
+    public Class1()
+    {
         // created new class1
         // create method for addition
         public int Add(int a, int b)
         {
             return a + b;
         }
+
+        // create method for diff
+        public int Diff(int a, int b)
+        { return a - b; }
 
     }
 }
