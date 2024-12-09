4348c841cb18f6c3ae6b975fc3be1d0c47aafa22
Pooja Kachhiya <pooja.kachhiya@dynatechconsultancy.com>
2024-12-09 11:45:50 +0530

    add add method

--- ProductWeight/ProductWeight/BusinessLogic/Class1.cs
+++ ProductWeight/ProductWeight/BusinessLogic/Class1.cs
@@ -4,6 +4,12 @@ public class Class1
 {
 	public Class1()
 	{
-		// created new class1
-	}
+        // created new class1
+        // create method for addition
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
 }
